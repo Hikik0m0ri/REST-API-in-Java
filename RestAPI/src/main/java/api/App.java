a8e5e8d05b77a8e4dfbf36427cd3473443b1b5ff
Artem Sarkisian <artems@localexpress.io>
2023-01-11 23:19:02 +0400

    server config

--- RestAPI/src/main/java/api/App.java
+++ RestAPI/src/main/java/api/App.java
@@ -1,7 +1,30 @@
 package api;
 
+import spark.Spark;
+
+import static spark.Spark.*;
+
 public class App {
+    public static final String JSON_MIME_TYPE = "application/json";
+    public static final String SERVER_HOST = "127.0.0.1";
+    public static final int SERVER_PORT = 8808;
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // server config
+        ipAddress(SERVER_HOST);
+        port(SERVER_PORT);
+
+        //routs
+        after((request, response) -> {
+            response.type(JSON_MIME_TYPE);
+        });
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run(){
+                //stop server
+                Spark.stop();
+            }
+        });
     }
 }
